a850289cdebfe89347ec1b3e284aa68874df30a9
timgrohmann <timgrohmann@icloud.com>
2016-06-11 12:14:41 +0200

    Initial Commit

---
+++ Vertretungsplan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vertretungsplan
+//
+//  Created by Tim Grohmann on 11.06.16.
+//  Copyright © 2016 Tim Grohmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
